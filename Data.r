d067cbcfae34b1fb2bfe05483e48b0ee70280baf
Zixuan Fan <ge43yeb@mytum.de>
2023-11-15 12:11:14 -0600

    added the ratios

--- Data.r
+++ Data.r
@@ -41,15 +41,20 @@ for(i in 1: length(users)){ # 1){ #
 
     tuning_of_action_time = var(user_logs$action_time[user_logs$activity == "Input"])
     tuning_time_space = rep(0, length(acticity)-1)
-    change = rep(0, length(acticity)-1)
-
-    for(j in 1:(length(acticity)-1)){
-        change = !(user_logs$activity[j+1] == user_logs$activity[j])
-    }
 
     for(j in 1:(length(acticity)-1)){
         tuning_time_space = user_logs$down_time[j+1] - user_logs$up_time[j]
     }
+
+    # ratios 
+    word_count_max = max(user_logs$word_count)
+    event_id_max = max(user_logs$event_id)
+    up_time_max = max(user_logs$up_time)
+    action_time_gap_sum = sum(tuning_time_space)
+    word_time_ratio = word_count_max / up_time_max
+    word_event_ratio = word_count_max / event_id_max
+    event_time_ratio = event_id_max  / up_time_max
+    idle_time_ratio = action_time_gap_sum / up_time_max
 
     train_1[i,2] <- as.numeric(train_scores[train_scores$id == user,2])
     train_1[i,3] <- as.numeric(present_of_Nonproduction)
@@ -60,6 +65,11 @@ for(i in 1: length(users)){ # 1){ #
     train_1[i,8] <- as.numeric(present_of_MoveFromTo)
     train_1[i,9] <- as.numeric(tuning_of_action_time)
     train_1[i,10] <- as.numeric(mean(tuning_time_space))
+    train_1[i, 11] <- as.numeric(change)
+    train_1[i, 12] <- as.numeric(word_time_ratio)
+    train_1[i, 13] <- as.numeric(word_event_ratio)
+    train_1[i, 14] <- as.numeric(event_time_ratio)
+    train_1[i, 15] <- as.numeric(idle_time_ratio)
 }
 
 print(colnames(train_1))
