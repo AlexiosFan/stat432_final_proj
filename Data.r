b7f634b071a1c4388118c1e62ad212110ea39128
MelchizedekM <rmin@connect.ust.hk>
2023-11-26 21:03:58 -0600

    RE

--- Data.r
+++ Data.r
@@ -1,4 +1,6 @@
 if (!require('this.path')) install.packages('this.path')
+if (!require('devtools')) install.packages('devtools')
+
 library(devtools)
 library(this.path)
 cur_dir2 = dirname(this.path())
@@ -12,9 +14,13 @@ length(users)
 # model (0 for whole set and save the data, 1 for whole set but not save the data, 2 for only the first user and not save the data)
 model = 0
 length_of_loop = length(users)
+first = 0
 if(model == 2) {
     length_of_loop = 1
     i = 1
+    # frist be a random user
+    first = sample(1:length(users), 1)
+    length_of_loop = 1
 }
 
 ## Processing
@@ -109,18 +115,40 @@ processingInputs <- function(currTextInput) {
   return(essayText)
 }
 
+calculateWordLengths <- function(text) {
+  # 使用正则表达式分割文本，包括空格、逗号、问号、句号、引号和分号
+  words <- unlist(strsplit(text, "[ ,.?\"';!\']+"))
+  # 移除空字符串
+  words <- words[words != ""]
+  # 计算每个单词的长度
+  wordLengths <- sapply(words, nchar)
+  return(wordLengths)
+}
+
+calculateParagraphCount <- function(text) {
+  # 使用 '\n' 分割文本为段落
+  paragraphs <- unlist(strsplit(text, "\n"))
+  # 移除长度为 0 的段落
+  paragraphs <- paragraphs[nchar(paragraphs) > 0]
+  # 计算段落数
+  paragraphCount <- length(paragraphs)
+  return(paragraphCount)
+}
+
 
 
 # the names of variables
 names_of_variables <- c("id","Score", "present_of_Nonproduction", "present_of_Input", "present_of_RemoveCut", "present_of_Paste", "present_of_Replace", "present_of_MoveFromTo",  
-                "tuning_of_action_time", "tuning_space_time", "fluquence_of_change", "word_time_ratio", "word_event_ratio","event_time_ratio","idle_time_ratio","tuning_sentence_length")
+                "tuning_of_action_time", "tuning_space_time", "fluquence_of_change", "word_time_ratio", "word_event_ratio","event_time_ratio","idle_time_ratio","tuning_sentence_length", "num_of_sentence"
+                , "tuning_word_length", "num_of_words", "num_of_paragraphs")
 MyDataSet <- matrix(0, nrow = length_of_loop, ncol = length(names_of_variables))
 colnames(MyDataSet) <- names_of_variables
 
 # begin processing
 print("processing began")
-for(i in 1: length_of_loop){ 
+for(k in 1 : length_of_loop){ 
 
+    i = k + first 
     # current user's name 
     user <- users[i]
     user_logs <- train_logs[train_logs$id == user,]
@@ -180,28 +208,44 @@ for(i in 1: length_of_loop){
     
     # number of sentences
     num_of_sentence = length(gregexpr("[.!?]", essayText)[[1]])
+
+    # sentence length
     sentence_length <- calculateSentenceLengths(essayText)
+
+    # tuning word length
+    word_length <- calculateWordLengths(essayText)
+
+    #num of words
+    num_of_words = length(word_length)
+    
+    # num of paragraphs
+    num_of_paragraphs <- calculateParagraphCount(essayText)
 
 
     # data pull in 
-    MyDataSet[i,1] <- user
-    MyDataSet[i,2] <- as.numeric(train_scores[train_scores$id == user,2])
-    MyDataSet[i,3] <- as.numeric(present_of_Nonproduction)
-    MyDataSet[i,4] <- as.numeric(present_of_Input)
-    MyDataSet[i,5] <- as.numeric(present_of_RemoveCut)
-    MyDataSet[i,6] <- as.numeric(present_of_Paste)
-    MyDataSet[i,7] <- as.numeric(present_of_Replace)
-    MyDataSet[i,8] <- as.numeric(present_of_MoveFromTo)
-    MyDataSet[i,9] <- as.numeric(tuning_of_action_time)
-    MyDataSet[i,10] <- as.numeric(mean(tuning_time_space)) # tuning_time_space
-    MyDataSet[i, 11] <- as.numeric(sum(fluquence_of_change)/length(fluquence_of_change)) #`fluquence_of_change
-    MyDataSet[i, 12] <- as.numeric(word_time_ratio) #`word_time_ratio
-    MyDataSet[i, 13] <- as.numeric(word_event_ratio) #`word_event_ratio
-    MyDataSet[i, 14] <- as.numeric(event_time_ratio) # tuning event time
-    MyDataSet[i, 15] <- as.numeric(idle_time_ratio) # tuning idle time
-    MyDataSet[i, 16] <- as.numeric(mean(sentence_length)) # tuning sentence length
+    MyDataSet[k,1] <- user
+    MyDataSet[k,2] <- as.numeric(train_scores[train_scores$id == user,2])
+    MyDataSet[k,3] <- as.numeric(present_of_Nonproduction)
+    MyDataSet[k,4] <- as.numeric(present_of_Input)
+    MyDataSet[k,5] <- as.numeric(present_of_RemoveCut)
+    MyDataSet[k,6] <- as.numeric(present_of_Paste)
+    MyDataSet[k,7] <- as.numeric(present_of_Replace)
+    MyDataSet[k,8] <- as.numeric(present_of_MoveFromTo)
+    MyDataSet[k,9] <- as.numeric(tuning_of_action_time)
+    MyDataSet[k,10] <- as.numeric(mean(tuning_time_space)) # tuning_time_space
+    MyDataSet[k, 11] <- as.numeric(sum(fluquence_of_change)/length(fluquence_of_change)) #`fluquence_of_change
+    MyDataSet[k, 12] <- as.numeric(word_time_ratio) #`word_time_ratio
+    MyDataSet[k, 13] <- as.numeric(word_event_ratio) #`word_event_ratio
+    MyDataSet[k, 14] <- as.numeric(event_time_ratio) # tuning event time
+    MyDataSet[k, 15] <- as.numeric(idle_time_ratio) # tuning idle time
+    MyDataSet[k, 16] <- as.numeric(mean(sentence_length)) # tuning sentence length
+    MyDataSet[k, 17] <- as.numeric(num_of_sentence) # number of sentences
+    MyDataSet[k, 18] <- as.numeric(mean(word_length)) # tuning word length
+    MyDataSet[k, 19] <- as.numeric(num_of_words) # number of words
+    MyDataSet[k, 20] <- as.numeric(num_of_paragraphs) # number of paragraphs
 
-    if(i == 1) {
+
+    if(k == 1) {
         # first user's data
         first_urser_data <- user_logs
     }
@@ -213,3 +257,4 @@ if(model != 2){
     save(MyDataSet, file = paste(cur_dir2, "/MyData.RData", sep = ""))
     save(users, file = paste(cur_dir2, "/users.RData", sep = ""))
 }
+
